e90d834643f403b024e0bcca1268aaa5c1884bed
Kyle <59634395+kyle1373@users.noreply.github.com>
2024-06-15 13:16:37 -0700

    Progress

--- archiverse/pages/index.tsx
+++ archiverse/pages/index.tsx
@@ -1,15 +1,91 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import SEO from "@/components/SEO";
 import styles from "./index.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <SEO />
-      <main className="">
+      <main className="relative container p-4 mx-auto">
+        <div className="flex flex-col md:flex-row md:space-x-4 max-w-[1200px] mx-auto">
+
+          <div className="hidden md:block w-[200px] bg-slate-800">
+            <div className="md:fixed md:w-[200px] bg-white p-4 mb-4 md:mb-0">
+              <h2 className="text-xl font-bold mb-4">Absolute</h2>
+              <p>Content for the left column goes here.</p>
+            </div>
+          </div>
+          {/* Right Column */}
+          <div className="md:w-[600px] bg-white p-4 border-[1px] rounded-md border-gray md:ml-auto">
+            <h2 className="text-2xl font-semibold mb-4 text-center text-green mt-8 md:mt-0">
+              Welcome to Archiverse!
+            </h2>
+            <p>
+              Content for the right column goes here. Add more content to see
+              the stacking effect on smaller screens.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id
+              dui at ipsum dictum tristique. Donec viverra sapien et tellus
+              pretium, eu convallis elit convallis. Curabitur bibendum, sapien
+              sit amet bibendum tincidunt, nulla leo dictum lorem, a pulvinar
+              erat odio a arcu. Aliquam erat volutpat. Fusce eu lorem eget dolor
+              euismod varius.
+            </p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+            <p>More content...</p>
+          </div>
+        </div>
       </main>
     </>
   );
--- archiverse/tailwind.config.ts
+++ archiverse/tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        green: "#3caa00",
+        gray: "#dddddd",
+      }
     },
   },
   plugins: [],
