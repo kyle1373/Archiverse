1a6d3280a6de72a6bd7003eaf1c652974308f3cc
Kyle <59634395+kyle1373@users.noreply.github.com>
2024-06-15 15:32:18 -0700

    Progress on homepage

--- archiverse/pages/index.tsx
+++ archiverse/pages/index.tsx
@@ -1,89 +1,59 @@
 import Image from "next/image";
 import SEO from "@/components/SEO";
 import styles from "./index.module.css";
+import { FaSearch } from "react-icons/fa";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <SEO />
-      <main className="relative container p-4 mx-auto">
-        <div className="flex flex-col md:flex-row md:space-x-4 max-w-[1200px] mx-auto">
-
-          <div className="hidden md:block w-[200px] bg-slate-800">
-            <div className="md:fixed md:w-[200px] bg-white p-4 mb-4 md:mb-0">
-              <h2 className="text-xl font-bold mb-4">Absolute</h2>
-              <p>Content for the left column goes here.</p>
+      <main className="container mx-auto p-4 py-10 max-w-[800px]">
+        <div className="flex flex-col md:flex-row md:space-x-4">
+          <div className="md:w-1/3">
+            <div className="md:fixed md:p-4 mb-4 md:mb-0">
+              <div className="flex justify-between md:flex-col">
+                <Link href="/">
+                  <img
+                    src={"/archiverse-logo.png"}
+                    alt={"archiverse logo"}
+                    className="w-40 md:w-52"
+                  />
+                </Link>
+                <Link className="md:mt-8 mt-[-20px]" href="/search">
+                  <div className="md:ml-2 hover:brightness-95 inline-flex justify-center items-center bg-gradient-to-b from-white border-[1px] rounded-md border-gray text-neutral-600 to-neutral-200 font-bold py-2 px-4 mt-4 md:mt-0 md:text-base text-small">
+                    <FaSearch />
+                    <h1 className="ml-3">Users</h1>
+                  </div>
+                </Link>
+              </div>
             </div>
           </div>
           {/* Right Column */}
-          <div className="md:w-[600px] bg-white p-4 border-[1px] rounded-md border-gray md:ml-auto">
-            <h2 className="text-2xl font-semibold mb-4 text-center text-green mt-8 md:mt-0">
+          <div className="md:w-2/3 bg-white p-4 border-[1px] rounded-md border-gray items-center md:mt-12">
+            <h2 className="text-xl font-semibold mb-4 text-center text-green mt-6">
               Welcome to Archiverse!
             </h2>
-            <p>
-              Content for the right column goes here. Add more content to see
-              the stacking effect on smaller screens.
+            <div className="flex justify-center mt-8">
+              <Image
+                src={"/welcome-image.png"}
+                alt={"welcome image"}
+                width={430}
+                height={204}
+              />
+            </div>
+            <p className="text-sm mt-6 text-neutral-700">
+              Archiverse is an archive of Miiverse, a social media platform for
+              the Nintendo Wii U and 3DS which ran from November 18, 2012 until
+              November 8, 2017. This archive stores millions of archived
+              Miiverse users, posts, drawings, comments, and more, totaling over
+              17TB of data.
             </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id
-              dui at ipsum dictum tristique. Donec viverra sapien et tellus
-              pretium, eu convallis elit convallis. Curabitur bibendum, sapien
-              sit amet bibendum tincidunt, nulla leo dictum lorem, a pulvinar
-              erat odio a arcu. Aliquam erat volutpat. Fusce eu lorem eget dolor
-              euismod varius.
-            </p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
-            <p>More content...</p>
+            <div className="mt-4 flex justify-between border-b-4 mx-[-16px] px-4 py-2 border-green">
+              <h1 className="text-green font-semibold sm:text-lg text-sm">
+                Communities
+              </h1>
+            </div>
           </div>
         </div>
       </main>
