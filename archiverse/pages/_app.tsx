5d2db5b503d040ba67561b27ca631a061624f10c
Kyle <59634395+kyle1373@users.noreply.github.com>
2024-06-15 16:58:46 -0700

    Added google adsense

--- archiverse/pages/_app.tsx
+++ archiverse/pages/_app.tsx
@@ -1,13 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Provider } from 'react-redux';
-import store from '../redux/store'
-
+import { Provider } from "react-redux";
+import store from "../redux/store";
+import { GoogleAdSense } from "nextjs-google-adsense";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />{" "}
-    </Provider>
+    <>
+      <GoogleAdSense publisherId="pub-4203889559099732" />
+      <Provider store={store}>
+        <Component {...pageProps} />{" "}
+      </Provider>
+    </>
   );
 }
--- archiverse/pages/_document.tsx
+++ archiverse/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4203889559099732"
+          crossOrigin="anonymous"
+        ></script>
+        <meta name="google-adsense-account" content="ca-pub-4203889559099732" />
+      </Head>
       <body>
         <Main />
         <NextScript />
