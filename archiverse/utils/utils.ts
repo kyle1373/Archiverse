78c0389467711ba108b11f7c3ef0039822643df2
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2024-07-06 16:13:31 -0700

    Added removeSpecialCharacters function

--- archiverse/utils/utils.ts
+++ archiverse/utils/utils.ts
@@ -13,3 +13,8 @@ export function extractEnglishCharacters(input: string): string {
   // Use a regular expression to replace all non-alphanumeric characters with an empty string
   return input.replace(/[^a-zA-Z0-9 ]/g, '');
 }
+
+export function removeSpecialCharacters(input: string): string {
+  // This regex keeps alphanumeric characters from various scripts and removes special characters
+  return input.replace(/[^\p{L}\p{N}\s\p{P}']/gu, '');
+}
