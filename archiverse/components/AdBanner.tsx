33faddb436e1beddc5c96856a9fe8bf868a91ac9
synthful <59634395+kyle1373@users.noreply.github.com>
2024-07-11 20:37:58 -0700

    Update AdBanner.tsx

--- archiverse/components/AdBanner.tsx
+++ archiverse/components/AdBanner.tsx
@@ -6,17 +6,7 @@ export default function AdBanner() {
     <div className="bg-gray py-2 text-center md:px-2 pt-1">
       <h1 className="text-neutral-500 text-xs mb-2">Advertisement</h1>
       <>
-        <amp-ad
-          width="100vw"
-          height="320"
-          type="adsense"
-          data-ad-client="ca-pub-4203889559099732"
-          data-ad-slot="1525315872"
-          data-auto-format="rspv"
-          data-full-width=""
-        >
-          <div overflow=""></div>
-        </amp-ad>
+        
       </>
     </div>
   );
