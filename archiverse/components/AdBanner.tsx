e6e5401cff08c317ca8bd630dc3b872d1d7d6e8c
synthful <59634395+kyle1373@users.noreply.github.com>
2024-07-12 00:07:15 -0700

    Update AdBanner.tsx

--- archiverse/components/AdBanner.tsx
+++ archiverse/components/AdBanner.tsx
@@ -3,7 +3,9 @@ import Script from "next/script";
 import { LiaWindowClose } from "react-icons/lia";
 
 export default function AdBanner() {
-//   const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
+  const [showCloseButton, setShowCloseButton] = useState(false);
+
   useEffect(() => {
     if (window && (window as any).adsbygoogle) {
       console.log("Loading ad inside useEffect");
@@ -16,29 +18,37 @@ export default function AdBanner() {
     }
   }, []);
 
-//   if (!isOpen) return <></>;
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowCloseButton(true);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isOpen) return <></>;
 
   return (
-    <div className="bg-gray py-2 text-center md:px-2">
+    <div className="relative bg-gray py-2 text-center md:px-2">
+      <div className="absolute top-0 right-0 mr-2 mt-[6px]">
+        {showCloseButton && (
+          <button onClick={() => setIsOpen(false)}>
+            <LiaWindowClose size={18} />
+          </button>
+        )}
+      </div>
       <div className="flex justify-center mb-2">
-        {/* <div></div> */}
         <h2 className="text-neutral-400 text-[10px]">
           Ads help maintain Archiverse's server costs
         </h2>
-        {/* <button className="mr-2" onClick={() => setIsOpen(false)}>
-          <LiaWindowClose size={18} />
-        </button> */}
       </div>
-      <>
-        <ins
-          className="adsbygoogle"
-          style={{ display: "block" }}
-          data-ad-client="ca-pub-4203889559099732"
-          data-ad-format="horizontal"
-          data-ad-slot="1525315872"
-          data-full-width-responsive="false"
-        ></ins>
-      </>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-4203889559099732"
+        data-ad-format="horizontal"
+        data-ad-slot="1525315872"
+        data-full-width-responsive="false"
+      ></ins>
     </div>
   );
 }
