8fe1c8c8a3094f31a1d10451bb01d11a72442f0c
synthful <59634395+kyle1373@users.noreply.github.com>
2024-07-11 20:25:35 -0700

    Update AdBanner.tsx

--- archiverse/components/AdBanner.tsx
+++ archiverse/components/AdBanner.tsx
@@ -1,23 +1,36 @@
-import { useEffect } from "react";
-import Script from "next/script";
+import { useAmp } from 'next/amp';
+import Script from 'next/script';
+
+export const config = { amp: 'hybrid' }; // Enable hybrid AMP mode
 
 export default function AdBanner() {
+  const isAmp = useAmp(); // Check if the page is being rendered as AMP
+
   return (
     <div className="bg-gray py-2 text-center md:px-2 pt-1">
       <h1 className="text-neutral-500 text-xs mb-2">Advertisement</h1>
-      <>
-        <amp-ad
-          width="100vw"
-          height="320"
-          type="adsense"
-          data-ad-client="ca-pub-4203889559099732"
-          data-ad-slot="1525315872"
-          data-auto-format="rspv"
-          data-full-width=""
-        >
-          <div overflow=""></div>
-        </amp-ad>
-      </>
+      {isAmp ? (
+        <>
+          <amp-ad
+            width="100vw"
+            height="320"
+            type="adsense"
+            data-ad-client="ca-pub-4203889559099732"
+            data-ad-slot="1525315872"
+            data-auto-format="rspv"
+            data-full-width=""
+          >
+            <div overflow=""></div>
+          </amp-ad>
+          <Script
+            async
+            custom-element="amp-ad"
+            src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"
+          />
+        </>
+      ) : (
+        <p>AMP ads will only be displayed on AMP pages.</p>
+      )}
     </div>
   );
 }
