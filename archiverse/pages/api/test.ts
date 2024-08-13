a3a7ac214d8d84e725bbf04bca5590cd149f597e
synthful <59634395+kyle1373@users.noreply.github.com>
2024-08-13 00:47:57 -0700

    Updated package.json

--- archiverse/pages/api/test.ts
+++ archiverse/pages/api/test.ts
@@ -1,23 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { SETTINGS } from "@constants/constants";
-import {
-  getCommunities,
-  getCommunity,
-  getHomepageDrawings,
-  getPost,
-  getPostReplies,
-  getUserInfo,
-  searchCommunities,
-  searchUsers,
-} from "@server/database";
+import { logServerStats } from "@server/logger";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  logServerStats(req, res);
+
   if (process.env.NODE_ENV !== "development") {
-    return res.status(403).json({error: "Not authorized"});
+    return res.status(403).json({ error: "Not authorized" });
   }
 
   if (SETTINGS.Maintenance) {
@@ -26,7 +19,11 @@ export default async function handler(
     });
   }
 
-  // const data = await getPost({postID: "AYIHAAAEAAAOU4XCvEQWvA"})
-  const data = await getHomepageDrawings();
-  return res.status(200).json(data);
+  const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  // Wait for 15 seconds (15000 milliseconds) to respect middleware's timeout
+  await wait(40000);
+
+  // Send a response after the wait is over
+  res.status(200).json({ message: "Waited for 15 seconds before responding" });
 }
