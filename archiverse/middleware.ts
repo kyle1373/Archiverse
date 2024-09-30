651153cb203cc2b7b0a3818a4aa4441bafd09832
Synthful <kylewade1373@gmail.com>
2024-09-30 07:00:12 -0700

    Added maintenance

--- archiverse/middleware.ts
+++ archiverse/middleware.ts
@@ -1,14 +1,37 @@
-import { NextResponse, NextRequest } from 'next/server';
+import { NextResponse, NextRequest } from "next/server";
 
 // Middleware function
 export async function middleware(req: NextRequest) {
-    const start = Date.now();
-  
-    const response = NextResponse.next();
-  
-    response.headers.set('X-Request-Start', start.toString());
-    response.headers.set('X-Logging', "true");
+  const { pathname } = req.nextUrl;
 
+  // Pass through requests for Next.js static assets
+  if (pathname.startsWith("/_next")) {
+    return NextResponse.next();
+  }
 
-    return response;
+  // Check if the request is for a static asset based on the file extension
+  const fileExtensionPattern = /\.(png|jpg|jpeg|gif|mp4|webm|svg)$/i;
+
+  if (fileExtensionPattern.test(pathname)) {
+    // If the request is for a file, allow it to proceed
+    return NextResponse.next();
+  }
+
+  const start = Date.now();
+
+  const response = NextResponse.next();
+  response.headers.set("X-Request-Start", start.toString());
+  response.headers.set("X-Logging", "true");
+
+  // Exclude the /maintenance path from the redirect
+  if (pathname !== "/maintenance") {
+    const maintenanceMode = true; // Toggle this based on your condition.
+
+    if (maintenanceMode) {
+      // Redirect to /maintenance
+      return NextResponse.redirect(new URL("/maintenance", req.nextUrl.origin));
+    }
+  }
+
+  return response;
 }
